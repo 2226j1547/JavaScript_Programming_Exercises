117f4e049bd7135ac8aedd350c72b047294d4294
2226j1547 <144420353+2226j1547@users.noreply.github.com>
2024-07-02 10:02:24 +0530

    Update Ch2_pgm_01.js

--- 02_JS_variables/Ch2_pgm_01.js
+++ 02_JS_variables/Ch2_pgm_01.js
@@ -1,16 +1,21 @@
 // Declaring a variable
-
 var score;
 
 /* Further Adventures
  *
- * 1) Delete the semicolon at after the word score.
+ * 1) Delete the semicolon after the word score.
  *    JS Bin should complain and show an error message.
  *    Take a look at the message.
  *
  * 2) Add the semicolon back after the word score.
- *    The semicolon shows the end of the statement.
+ *    The semicolon indicates the end of the statement in JavaScript.
  *
  * 3) Declare two more variables.
- *
+ *    You can declare additional variables by following the same pattern.
  */
+
+
+var score;
+var playerName;
+var highScore;
+
