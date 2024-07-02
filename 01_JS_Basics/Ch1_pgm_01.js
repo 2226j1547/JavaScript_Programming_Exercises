a6ba4ea73ca0746827bb9c2362f139ad1492c16f
2226j1547 <144420353+2226j1547@users.noreply.github.com>
2024-07-02 10:00:51 +0530

    Update Ch1_pgm_01.js

--- 01_JS_Basics/Ch1_pgm_01.js
+++ 01_JS_Basics/Ch1_pgm_01.js
@@ -1,18 +1,22 @@
 // Using console.log to display information
-
 console.log("Hello World!");
-
 
 /* Further Adventures
  *
  * 1) Change the text between the parentheses.
+ *    This allows you to modify the message that gets printed to the console.
  *
  * 2) Click Run on the console to run the program again.
+ *    After making changes, click Run to see the updated output in the console.
  *
  * 3) Add your own console.log statement
  *    to the program, so there are two.
- *    Don't forget the semicolon.
+ *    Don't forget the semicolon at the end of each statement.
+ *    This step encourages you to experiment with adding more messages to the output.
  *
  * 4) Click Run.
+ *    Once you've made changes or additions, click Run to see the effects in the console.
  *
  */
+console.log("Hello JavaScript!");
+// Click Run to see the output in the console after making these changes.
